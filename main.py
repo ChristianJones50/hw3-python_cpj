478bc6fed968cee37fb06592b84b289664d39525
Christian Jones <cpj5199@psu.edu>
2020-09-17 20:33:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Author: Christian Jones cpj5199@psu.edu
+
+def digit_sum(n):
+  if n > 0:
+    digit = int(n % 10) # Stores the Last Digit
+    n = n // 10
+    return digit + digit_sum(n)
+  else:
+    return 0
+
+def run():
+  intMain = int(input("Enter an int: "))
+  print(f"sum of digits {intMain} is {digit_sum(intMain)}.")
+
+if __name__ == "__main__":
+  run()
